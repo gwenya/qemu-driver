ddf0dbaceb1c5e6e09ed951d486921f1313812cd
Gwendolyn <me@gwendolyn.dev>
2025-12-30 17:16:26 +0100

    remove cgo

--- driver/vsock.go
+++ driver/vsock.go
@@ -9,10 +9,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-/*
-#include <linux/vhost.h>
-*/
-import "C"
+const VHOST_VSOCK_SET_GUEST_CID = 0x4008AF60
 
 func openVsock(cid uint32) (retF *os.File, retErr error) {
 	if cid == 1 || cid == 2 || cid == 0xffffffff {
@@ -32,7 +29,7 @@ func openVsock(cid uint32) (retF *os.File, retErr error) {
 
 	cidUint64 := uint64(cid)
 
-	_, _, errno := unix.Syscall(unix.SYS_IOCTL, f.Fd(), C.VHOST_VSOCK_SET_GUEST_CID, uintptr(unsafe.Pointer(&cidUint64)))
+	_, _, errno := unix.Syscall(unix.SYS_IOCTL, f.Fd(), VHOST_VSOCK_SET_GUEST_CID, uintptr(unsafe.Pointer(&cidUint64)))
 	if errno != 0 {
 		if errors.Is(errno, unix.EADDRINUSE) {
 			return nil, fmt.Errorf("vsock cid %d is already in use: %w", cid, errno)
